fd3afa5f63837e17dfc406f267aef358938b7427
Nasser Sayeh <nasser.sayeh89@gmail.com>
2021-06-10 00:16:06 +0300

    Lists of exceptions

---
+++ Java_fundemantals/exceptionsHandling/exceptionsHandling.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.HashMap;
+import java.util.Set;
+
+public class exceptionsHandling {
+public static void main(String [] args){
+ArrayList<Object> myList = new ArrayList<Object>();
+myList.add("13");
+myList.add("hello world");
+myList.add(48);
+myList.add("Goodbye World");
+for(int i = 0; i < myList.size(); i++) {
+    try{
+    Integer castedValue = (Integer) myList.get(i);
+    }catch(ClassCastException e){
+        System.out.println("there is a Error Message in "+myList.indexOf(i)+" and the Value is : "+myList.get(i));
+    }
+}
+}
+
+}

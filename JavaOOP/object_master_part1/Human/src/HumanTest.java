f67e45f6813b134cf0bc110f453cbc737950ea82
Nasser Sayeh <nasser.sayeh89@gmail.com>
2021-06-10 15:59:03 +0300

    Object Master Part1

---
+++ JavaOOP/object_master_part1/Human/src/HumanTest.java
@@ -0,0 +1,13 @@
+
+public class HumanTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Human nasser = new Human();
+		Human ahmad = new Human();
+		nasser.attack(ahmad);
+		System.out.println(ahmad.health);
+
+	}
+
+}

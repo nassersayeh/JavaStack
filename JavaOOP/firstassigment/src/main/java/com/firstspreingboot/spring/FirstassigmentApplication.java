9a4c00a6049eebf1a105cd722beec96a0a777977
Nasser Sayeh <nasser.sayeh89@gmail.com>
2021-06-15 11:59:25 +0300

    Strings

---
+++ JavaOOP/firstassigment/src/main/java/com/firstspreingboot/spring/FirstassigmentApplication.java
@@ -0,0 +1,26 @@
+package com.firstspreingboot.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class FirstassigmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstassigmentApplication.class, args);
+	}
+	@RequestMapping("/")
+    // 3. Method that maps to the request route above
+    public String hello() { // 3
+            return "Hello Client! How are you doing ?";
+    }
+	@RequestMapping("/welcome")
+    // 3. Method that maps to the request route above
+    public String hello2() { // 3
+            return "Spring Boot is great! So easy to just respond with strings";
+    }
+}
---
+++ JavaOOP/firstassigment/src/test/java/com/firstspreingboot/spring/FirstassigmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstspreingboot.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstassigmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

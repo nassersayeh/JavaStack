1196f28e2e95c2b1fa1fee1b05ea3b69715476a0
Nasser Sayeh <nasser.sayeh89@gmail.com>
2021-06-16 16:54:23 +0300

    add the final requerment

--- SpringBoot/Counter/src/main/java/com/example/Counter.java
+++ SpringBoot/Counter/src/main/java/com/example/Counter.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Counter {
 	int count1=0;
+	int count2=0;
 	@RequestMapping("/counter")
 	public String index(HttpSession session){
         session.setAttribute("count", count1++);
@@ -16,6 +17,11 @@ public class Counter {
 	@RequestMapping("/")
 	public String index2(HttpSession session){
         return "Page2.jsp";
+    }
+	@RequestMapping("/counters")
+	public String index3(HttpSession session){
+        session.setAttribute("count2", count2+=2);
+        return "page3.jsp";
     }
 
 }
